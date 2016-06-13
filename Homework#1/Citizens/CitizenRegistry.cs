4946de0372b8717573ab394e4646984a0b3a6d1a
Paul Yerzhykevych <bizmonto@gmail.com>
2016-06-13 20:25:31 +0300

    Now VAT generator create different VAT for citizens with the same date of birth

--- Homework#1/Citizens/CitizenRegistry.cs
+++ Homework#1/Citizens/CitizenRegistry.cs
@@ -79,9 +79,14 @@ namespace Citizens
         {
             string idBirthDate = (birthDate.ToOADate() - 1).ToString();
             idBirthDate = new string('0', 5 - idBirthDate.Length) + idBirthDate;
+
             int idNumber = 0;
 
-            int maxNumber = 0;
+            int maxNumber = -2;
+            if (gender == Gender.Male)
+            {
+                maxNumber = -1;
+            }
 
             for (int i = 0; i < count; i++)
             {
@@ -89,25 +94,18 @@ namespace Citizens
                 if (vatId.StartsWith(idBirthDate))
                 {
                     idNumber = int.Parse(vatId.Substring(5, 4));
-                    if (idNumber % 2 == (int)gender + 1 && idNumber > maxNumber)
+                    if (idNumber > maxNumber && idNumber % 2 == ((int)gender + 1) % 2)
                     {
                         maxNumber = idNumber;
                     }
                 }
             }
 
-            if (idNumber == 0)
-            {
-                idNumber = (int)gender + 1;
-            }
-            else
-            {
-                idNumber += 2;
-            }
+            idNumber = maxNumber + 2;
 
             string idNumberString = idNumber.ToString();
             idNumberString = new string('0', 4 - idNumberString.Length) + idNumberString;
-            int idChecksum = int.Parse(idBirthDate[0].ToString()) * (-1) + int.Parse(idBirthDate[1].ToString()) * 5 + int.Parse(idBirthDate[2].ToString()) * 7 + int.Parse(idBirthDate[4].ToString()) * 9 + int.Parse(idBirthDate[4].ToString()) * 4 + int.Parse(idNumberString[0].ToString()) * 6 + int.Parse(idNumberString[1].ToString()) * 10 + int.Parse(idNumberString[2].ToString()) * 5 + int.Parse(idNumberString[3].ToString()) * 7;
+            int idChecksum = (int.Parse(idBirthDate[0].ToString()) * (-1)) + (int.Parse(idBirthDate[1].ToString()) * 5) + (int.Parse(idBirthDate[2].ToString()) * 7) + (int.Parse(idBirthDate[4].ToString()) * 9) + (int.Parse(idBirthDate[4].ToString()) * 4) + (int.Parse(idNumberString[0].ToString()) * 6) + (int.Parse(idNumberString[1].ToString()) * 10) + (int.Parse(idNumberString[2].ToString()) * 5) + (int.Parse(idNumberString[3].ToString()) * 7);
 
             return idBirthDate + idNumberString + idChecksum;
         }
